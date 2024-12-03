8e0ae9985d1262359d79d9308d3bfc92e1bbac74
Johnny Gorman <jonathan.gorman@truecaller.com>
2024-12-03 23:48:23 +0100

    Add Day 2 - part 1 solution

---
+++ src/main/java/com/jonathandgorman/days/Day2.java
@@ -0,0 +1,64 @@
+package com.jonathandgorman.days;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+        try {
+            var input = Files.readAllLines(Paths.get("src/main/resources/day2.txt"));
+
+            // PART 1
+            var safeReports = 0;
+            for (String report : input) {
+                if (isValidReport(report)) safeReports++;
+            }
+            System.out.println("The total number of safe reports found is: " + safeReports);
+
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.err.println("Input error: " + e.getMessage());
+        }
+    }
+
+    static boolean isValidReport(String report) {
+
+        var splitReportLevels = Arrays.stream(report.split(" "))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        boolean isAscending = true;
+        for (int i = 0; i < splitReportLevels.size() - 1; i++) {
+            if (splitReportLevels.get(i) < splitReportLevels.get(i + 1)) {
+                isAscending = false;
+                break;
+            }
+        }
+
+        boolean isDescending = true;
+        for (int i = 0; i < splitReportLevels.size() - 1; i++) {
+            if (splitReportLevels.get(i) > splitReportLevels.get(i + 1)) {
+                isDescending = false;
+                break;
+            }
+        }
+
+        boolean isValidStep = true;
+        for (int i = 0; i < splitReportLevels.size() - 1; i++) {
+            int diff = Math.abs(splitReportLevels.get(i) - splitReportLevels.get(i + 1));
+            if (diff <= 0 || diff > 3) {
+                isValidStep = false;
+                break;
+            }
+        }
+
+        return isValidStep && (isDescending || isAscending);
+    }
+}
